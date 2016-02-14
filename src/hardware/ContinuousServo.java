bd83be6d1bed2eca3f7249e13f1af02b27e9890a
laneas <ardjenlane@hotmail.com>
2016-02-14 16:26:52 -0500

    Addition of ContinuousServo Class

---
+++ src/hardware/ContinuousServo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hardware;
+
+/**
+ *
+ * @author Ardjen
+ */
+public class ContinuousServo extends Servo implements Incrementable
+{
+    
+    public ContinuousServo(int thePin)
+    {
+        super(thePin);
+        upperLimit = 180;
+        lowerLimit = -180;
+        defaultAngle = 0;
+    }
+    
+}
